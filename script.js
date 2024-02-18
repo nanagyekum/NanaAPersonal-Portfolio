a588360eab502d127e82dbe6d9c768ef70cb6d6e
Nana Agyekum <nanagyekum5859@gmail.com>
2024-02-18 14:33:58 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".alotText", {
+  strings: [
+    "a Frontend Developer",
+    "an Actress",
+    "a Public Speaker",
+    "a TV Personality",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
